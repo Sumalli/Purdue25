15607e1d8df3db65a3fc1a7d334f6511ff6847f9
Gitbash <Gitbash@git.com>
2023-06-02 16:06:06 -0700

    more features were added

--- 5.java
+++ 5.java
@@ -1,5 +1,6 @@
 hi hello,how r u edureka team.
 added more features to feature branch. 
 changes made in defect branch.
-fixed some more changes in defect branch
+fixed some more changes in defect branch.
+added more features to java file.
 
